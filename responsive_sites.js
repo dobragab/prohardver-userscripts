51f0dd0365ed6c3596635123d8cc4968c8f30956
Gábor Dobra <dobragab@users.noreply.github.com>
2017-12-14 22:34:05 +0100

    Update responsive_sites.js

--- responsive_sites.js
+++ responsive_sites.js
@@ -1,6 +1,5 @@
-    function ph_is_site_responsive(hostname)
-    {
-        var sites = ["fototrend.hu"];
-        return sites.includes(hostname);
-    }
-    
+function ph_is_site_responsive(hostname)
+{
+    var sites = ["fototrend.hu"];
+    return sites.includes(hostname);
+}
