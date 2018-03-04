0658bb7d4c53de4c6abe38556304c546ce1efe47
milangfx <milangfx@gmail.com>
2018-03-04 18:01:26 +0100

    Update PH_comment_backgroud.user.js

--- PH_comment_backgroud.user.js
+++ PH_comment_backgroud.user.js
@@ -1,15 +1,10 @@
 // ==UserScript==
 // @name         PH_comment_backgroud
 // @namespace    https://www.prohardver.hu/
-// @match        https://prohardver.hu/forum/*
 // @match        https://prohardver.hu/tema/*
-// @match        https://mobilarena.hu/forum/*
 // @match        https://mobilarena.hu/tema/*
-// @match        https://logout.hu/forum/*
-// @match        https://logout.hu/tems/*
-// @match        https://itcafe.hu/forum/*
+// @match        https://logout.hu/tema/*
 // @match        https://itcafe.hu/tema/*
-// @match        https://gamepod.hu/forum/*
 // @match        https://gamepod.hu/tema/*
 // @version      0.1
 // @description  A sajat es valasz hsz-ek szinenek valtoztatasa
